5d694460553d0d7f49cdcdf760cb1d6078fd1356
Gianobre <87166317+Gianobre@users.noreply.github.com>
2021-07-10 12:55:48 +0800

    Add files via upload

---
+++ WK3_ASSIGNMENT.R
@@ -0,0 +1,23 @@
+makeCashematrix <- function(x = matrix()){
+  n <- NULL   #setting the variable "n" as NULL
+  set <- function (y){
+    x <<- y
+    n <<- NULL 
+  }
+  get <- function() {x}
+  setInverse <- function (inverse) {n <<- inverse}
+  getInverse <- function () {n}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+cacheSolve <- function(x, ...){    #Trying to obtain the cache data from the given present data
+  n <- x$getInverse()
+  if (!is.null(n)){                #Checking if the inverse function is NULL
+    message("obtaining cache data")  
+    return(n)                      #This function returns the inverse value of "n"
+  }
+  mat <- x$get()
+  n <- solve(mat, ...)
+  x$setInverse(n)
+  n  ##returning the matrix that is the inverse of "x"
+}
